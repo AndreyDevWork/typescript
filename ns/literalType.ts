c44bcb8115cfb8d5e630b56d7a04dbc63e8c0671
Andrey <andrey.develop.a@gmail.com>
2024-01-04 19:30:56 +0700

    Псевдонимы типов

--- ns/literalType.ts
+++ ns/literalType.ts
@@ -7,15 +7,17 @@ function startServer(protocol: 'http' | 'https', port: 3000 | 3001): 'Server sta
 
 console.log(startServer('https', 3000))
 
+type AnimationTimingFunc = 'ease' | 'ease-out' | 'ease-in' // Псведоним типов
+type AnimationId = string | number
 
 function createAnime(
-    id: string | number,
+    AnimationId: string | number,
     animName: string,
-    timing: 'ease' | 'ease-out' | 'ease-in' = 'ease',
+    timing: AnimationTimingFunc = 'ease',
     duration: number,
     iterCount: 'infinite' | number
     ): void {
-    const elem = document.querySelector(`#${id}`) as HTMLElement
+    const elem = document.querySelector(`#${AnimationId}`) as HTMLElement
     if(elem) {
         elem.style.animation = `${animName} ${timing} ${duration} ${iterCount}`
     }
