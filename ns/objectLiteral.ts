7a1b75ae01a1c3352db28111b2de6f95c220e9e8
Andrey <andrey.develop.a@gmail.com>
2024-01-06 22:31:29 +0700

    Пересечение типаов

--- ns/objectLiteral.ts
+++ ns/objectLiteral.ts
@@ -7,3 +7,38 @@ const serverConfig: {
 }
 
 
+
+
+
+
+
+type Config = {
+    protocol: 'https' | 'http',
+    port: 3001 | 3000,
+}
+type Role = {
+    role: string
+}
+type ConfigWithRole = Config & Role // Пересечение типаов (intersection) (соеденяет в одно)
+
+const serverCon: ConfigWithRole = {
+    protocol: 'https',
+    port: 3001,
+    role: 'admin'
+}
+
+
+const backupCon: ConfigWithRole = {
+    protocol: 'https',
+    port: 3000,
+    role: 'sisAdmin'
+}
+
+
+
+type StartFunction = (protocol: 'http' | 'https', port: 3000 | 3001) => string
+
+const startServerr: StartFunction = (protocol: 'http' | 'https', port: 3000 | 3001): "serverStarted" => {
+    console.log('serverStarted')
+    return "serverStarted"
+}
